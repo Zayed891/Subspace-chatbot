64f52e2ae25c343fdcdee9a4190502597f18be7d
Jayed99 <93325599+Zayed891@users.noreply.github.com>
2025-08-17 00:57:28 +0530

    Updated ChatArea.tsx

--- src/components/chat/ChatArea.tsx
+++ src/components/chat/ChatArea.tsx
@@ -72,14 +72,14 @@ export default function ChatArea({ chatId }: ChatAreaProps) {
   }
 
   return (
-    <div className="flex-1 flex flex-col bg-white">
+    <div className="flex-1 flex flex-col overflow-y-auto bg-white">
       <div className="border-b border-gray-200 p-4">
         <h1 className="text-lg font-semibold text-gray-900">Chat</h1>
       </div>
 
-      <div className="flex-1 overflow-y-auto px-4">
-        <MessageList messages={messages} isLoading={isLoading} />
-      </div>
+      
+      <MessageList messages={messages} isLoading={isLoading} />
+      
 
       <MessageInput onSendMessage={handleSendMessage} isLoading={isLoading} />
     </div>
